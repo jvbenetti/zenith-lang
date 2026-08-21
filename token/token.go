59bdfe7d5654e6de738ffc5a1fbbad0d50d30a1a
JaoVicy <filipimjoa@gmail.com>
2026-08-20 21:16:54 -0300

    feat: creating tokens structures

--- token/token.go
+++ token/token.go
@@ -1,1 +1,36 @@
 package token
+
+type TokenType string
+
+// Token é a estrutura que o Lexer vai gerar.
+// Ele guarda o tipo (ex: "+") e o valor literal exato que foi digitado (ex: "+").
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Dict to zenith can understand
+const (
+	ILLEGAL = "ILLEGAL" // simbols that the language dont understood
+	EOF     = "EOF"     // End Of File
+
+	// IDENT and INT
+	IDENT = "IDENT" // Vars name: x, y, some, result
+	INT   = "INT"   // Numbers: 1, 2, 100
+
+	// ASSIGN and PLUS Math operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// COMMA and SEMICOLON Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// FUNCTION and LET Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
