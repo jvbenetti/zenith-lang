181b3d87e6b7107b2a0bf98d55eb8fb3831e6cfb
JaoVicy <filipimjoa@gmail.com>
2026-08-25 20:58:59 -0300

    feat: creating Lexer struct

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,11 @@
 package lexer
+
+import "github.com/jvbenetti/zenith-lang.git/token"
+
+// Lexer save actually status
+type Lexer struct {
+	input        string
+	position     int  // Actually position of string
+	readPosition int  // Next read position (actually position + 1)
+	ch           byte // Actually ch in analyzes
+}
